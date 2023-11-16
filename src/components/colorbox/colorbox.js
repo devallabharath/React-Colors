ec468bfa0cf5f9f5ff69f128241a122b66473a68
Devalla <devallabharath619@gmail.com>
2023-11-16 16:49:26 -0500

    feat: ✨ Copy Color to Clipboard. Added 'react-copy-to-clipboard' dependency. Copy to Clipboard feature in ColorBox. Style changes in ColorBox.

--- src/components/colorbox/colorbox.js
+++ src/components/colorbox/colorbox.js
@@ -1,16 +1,19 @@
 import { PureComponent } from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard'
 
 class ColorBox extends PureComponent {
 
   render () {
     const { name, color } = this.props
     return (
-      <div style={{background:color}} className="ColorBox">
+      <div style={{ background: color }} className="ColorBox">
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
           </div>
-          <button className="copy-button">Copy</button>
+          <CopyToClipboard text={color}>
+            <button className="copy-button">Copy</button>
+          </CopyToClipboard>
         </div>
         <span className='see-more'>More</span>
       </div>
