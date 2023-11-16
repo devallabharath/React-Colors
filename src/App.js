1a309349eefd6f3cfec2c573bb3e7353e42cc6b7
Devalla <devallabharath619@gmail.com>
2023-11-16 12:15:28 -0500

    Project Initialization
    React Colors app initial template.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Colors</h1>
+      </div>
+    )
+  }
+}
+
+export default App
