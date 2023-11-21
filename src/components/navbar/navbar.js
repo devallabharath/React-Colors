eb3023ddecc89a1f7ce927ac904004807c322429
Bharath <devallabharath619@gmail.com>
2023-11-21 00:26:47 -0500

    feat: ✨ Home page Navbar. Modified Navbar to compatable for Home page. Added some styles.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,36 +5,57 @@ import './navbar.css'
 
 class Navbar extends PureComponent {
   render () {
-    const { Name, Format, changeFormat, slider, Level, changeLevel } = this.props;
+    const { Name, Format, changeFormat, home, slider, Level, changeLevel } = this.props;
     return (
       <nav>
-        <div className="top">
-          <div className='nav-part'>
-            <button className='back-button' onClick={this.props.history.goBack}>
-              <SlIcon name='arrow-left-circle-fill' />
-            </button>
-            <button className='home-button' onClick={() => this.props.history.push('/')}>
-              <SlIcon name='house-fill' />
-            </button>
+        {home
+          ?this.home(Name)
+          :<div className="top">
+            <div className='nav-part'>
+              <button className='back-button' onClick={this.props.history.goBack}>
+                <SlIcon name='arrow-left-circle-fill' />
+              </button>
+              <button className='home-button' onClick={() => this.props.history.push('/')}>
+                <SlIcon name='house-fill' />
+              </button>
+            </div>
+            <h4>{Name}</h4>
+            <SlSelect className='color-format' value={Format}
+              size='small' onSlChange={changeFormat}>
+              <SlOption value="hex"> Hex </SlOption>
+              <SlOption value="rgb"> RGB </SlOption>
+              <SlOption value="rgba"> RGBA </SlOption>
+            </SlSelect>
           </div>
-          <h4>{Name}</h4>
-          <SlSelect className='color-format' value={Format}
-            size='small' onSlChange={changeFormat}>
-            <SlOption value="hex"> Hex </SlOption>
-            <SlOption value="rgb"> RGB </SlOption>
-            <SlOption value="rgba"> RGBA </SlOption>
-          </SlSelect>
-        </div>
-        {slider && <div className="bottom">
-          <Slider className='Slider' min={100} max={900} step={100}
-            marks={{
-              100: '100', 200: '200', 300: '300', 400: '400',
-              500: '500', 600: '600', 700: '700', 800: '800', 900: '900'
-            }}
-            included={false} defaultValue={Level} onChange={changeLevel} />
-        </div>
         }
-      </nav>
+        {slider && this.slider(Level, changeLevel)}
+      </nav >
+    )
+  }
+
+  home = (Name) => {
+    return (
+      <div className="top">
+        <div className='nav-part'>
+          {/* //todo: logo */}
+          <h3>React Colors</h3>
+        </div>
+        <h4>{Name}</h4>
+        <span className='empty-holder'></span>
+      </div>
+    )
+  }
+
+  slider = (Level, changeLevel) => {
+    return (
+      <div className="bottom">
+        <Slider className='Slider' min={100} max={900} step={100}
+          marks={{
+            100: '100', 200: '200', 300: '300', 400: '400',
+            500: '500', 600: '600', 700: '700', 800: '800', 900: '900'
+          }}
+          included={false} defaultValue={Level} onChange={changeLevel} />
+      </div>
     )
   }
 
