3ab9c3b379269828543cd518810218260b995e6f
Bharath <devallabharath619@gmail.com>
2023-11-24 13:08:13 -0500

    feat: ✨ Finished New Palette page. Implemented save feature in the New Palette page. Updated Home page to get user stored palettes.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,4 @@
 import { PureComponent } from 'react'
-import Colors from '../scripts/colors'
 import Navbar from '../components/navbar'
 import MiniPalette from '../components/miniPalette'
 import '../styles/home.css'
@@ -7,12 +6,12 @@ import '../styles/home.css'
 class Home extends PureComponent {
 
   render () {
-    const {history} = this.props
+    const {history, palettes} = this.props
     return (
       <div className="Home">
         <Navbar Type='home' history={history} />
         <div className="home-palettes">
-          {Colors.map(c => <MiniPalette key={c.id} palette={c} />)}
+          {palettes.map(c => <MiniPalette key={c.id} palette={c} />)}
         </div>
       </div>
     )
