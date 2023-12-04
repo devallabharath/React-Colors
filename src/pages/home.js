09604581d969b34e7f6b5ed87aeaad3cb19a7d70
Bharath <devallabharath619@gmail.com>
2023-12-04 14:23:12 -0500

    feat: ✨ Implemented delete dialog in Home page.

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,28 +2,51 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../components/navbar'
 import MiniPalette from '../components/miniPalette'
+import Dialog from '../components/dialog'
 import '../styles/home.css'
 
 const HomePage = ({ Storage }) => {
   const [, refresh] = useState(false)
+  const [DeleteDlg, setDeleteDlg] = useState(false)
+  const [Current, setCurrent] = useState([])
   const navigate = useNavigate()
 
   function render () {
     return (<div className="Home">
       <Navbar Type='home' navigate={navigate} />
       <div className="home-palettes">
+        <Dialog
+          Type='YesNo'
+          Label='Are you sure?'
+          Content='This action will delete the palette...'
+          Display={DeleteDlg}
+          id={Current[0]}
+          YesName='Delete'
+          YesVariant='danger'
+          Yes={deletePalette}
+          NoName='Cancle'
+          NoVariant='secondary'
+          No={()=>setDeleteDlg(false)}
+        />
         {Storage.palettes.map(c =>
           <MiniPalette key={c.id}
             palette={c}
-            Delete={deletePalette}
+            Delete={openDeleteDlg}
           />
         )}
       </div>
     </div>)
   }
 
-  const deletePalette = (id) => {
+  const openDeleteDlg = (id) => {
+    setCurrent([id])
+    setDeleteDlg(true)
+  }
+
+  const deletePalette = () => {
+    const id = Current[0]
     Storage.deletePalette(id)
+    setDeleteDlg(false)
     refresh((old) => !old)
   }
 
