eaae6155f23897f9904311124836a3c30311d3af
Bharath <devallabharath619@gmail.com>
2023-12-04 17:03:47 -0500

    feat: ✨ Implemented Recycle Bin and some improvements. Added Recycle bin to the Storage. More usefull functions for accessing the hidden and deleted palettes.

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -3,8 +3,9 @@ import Colors from './colors'
 class Storage {
   constructor (props) {
     // localStorage.clear()
-    if(!localStorage.getItem('setup-done')) this.initialSetup()
+    if (!localStorage.getItem('setup-done')) this.initialSetup()
     this.hidden = JSON.parse(localStorage.getItem('hidden'))
+    this.deleted = JSON.parse(localStorage.getItem('deleted'))
     this.palettes = []
     this.getPalettes()
   }
@@ -13,11 +14,23 @@ class Storage {
     localStorage.setItem('setup-done', true)
     localStorage.setItem('palettes', JSON.stringify(Colors))
     localStorage.setItem('hidden', '[]')
+    localStorage.setItem('deleted', '[]')
   }
 
   getPalettes = () => {
     const local = JSON.parse(localStorage.getItem('palettes'))
-    this.palettes = local.filter((p)=>!this.hidden.includes(p.id))
+    const temp = [...this.hidden, ...this.deleted]
+    this.palettes = local.filter((p) => !temp.includes(p.id))
+  }
+
+  getHiddenPalettes = () => {
+    const local = JSON.parse(localStorage.getItem('palettes'))
+    return local.filter((p)=>this.hidden.includes(p.id))
+  }
+
+  getDeletedPalettes = () => {
+    const local = JSON.parse(localStorage.getItem('palettes'))
+    return local.filter((p)=>this.deleted.includes(p.id))
   }
 
   getPaletteNames () { return this.palettes.map(c => c.paletteName) }
@@ -40,17 +53,31 @@ class Storage {
 
   showPalette = (id) => {
     const hidden = JSON.parse(localStorage.getItem('hidden'))
-    const temp = hidden.filter((pid)=>pid!==id)
+    const temp = hidden.filter((pid) => pid !== id)
     localStorage.setItem('hidden', JSON.stringify(temp))
     this.hidden = temp
     this.getPalettes()
   }
 
   deletePalette = (id) => {
-    const local = JSON.parse(localStorage.getItem('palettes')).filter(p => p.id !== id)
-    const state = this.palettes.filter(p => p.id !== id)
-    localStorage.setItem('palettes', JSON.stringify(local))
-    this.palettes = state
+    const temp = [...this.deleted, id]
+    localStorage.setItem('deleted', JSON.stringify(temp))
+    this.deleted = temp
+    this.getPalettes()
+  }
+
+  restorePalette = (id) => {
+    const temp = this.deleted.filter((pid)=>pid!==id)
+    localStorage.setItem('deleted', JSON.stringify(temp))
+    this.deleted = temp
+  }
+
+  deleteFromBin = (id) => {
+    const local = JSON.parse(localStorage.getItem('palettes'))
+    const temp = local.filter(p => p.id !== id)
+    localStorage.setItem('palettes', JSON.stringify(temp))
+    const deleted = JSON.parse(localStorage.getItem('deleted'))
+    localStorage.setItem('deleted', deleted.filter((pid)=>pid!==id))
   }
 }
 
