493cdb6de1b3f31fe6f4a544d129274b4d9a9b0b
Bharath <devallabharath619@gmail.com>
2023-12-14 00:47:32 -0500

    refactor: ♻️ Refactored the Minipalette and its styles. Changed to functional component. Improved styles.

--- src/components/miniPalette.js
+++ src/components/miniPalette.js
@@ -1,22 +1,19 @@
-import { PureComponent, createRef } from 'react'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { SlDropdown, SlMenu, SlMenuItem, SlIcon } from '@shoelace-style/shoelace/dist/react'
 import '../styles/miniPalette.css'
 
-class MiniPalette extends PureComponent {
-  constructor (props) {
-    super(props)
-    this.current = createRef()
-  }
+const MiniPalette = (props) => {
+  const ref = useRef()
 
-  render () {
-    const { paletteName, colors, id } = this.props.palette
-    const { Type, Favs } = this.props
+  const render = () => {
+    const { paletteName, colors, id } = props.palette
+    const { Type, Favs } = props
     return (
       <div className="MiniPalette">
         <Link className='goto' to={`/palettes/${id}`}></Link>
-        {this.leftIcon(id)}
-        {this.rightIcon(id)}
+        {leftIcon(id)}
+        {rightIcon(id)}
         <div className="minipalette-colors">
           {colors.map(c =>
             <span
@@ -27,67 +24,63 @@ class MiniPalette extends PureComponent {
         </div>
         <div className="minipalette-footer">
           <div className='minipalette-name'>{paletteName}</div>
-          {Type === 'home' && this.loveIcon(id, Favs)}
+          {Type === 'home' && loveIcon(id, Favs)}
         </div>
       </div >
     )
   }
 
-  loveIcon = (id, Favs) => {
-    const iconId = `love${id}`
-    const iconClass = Favs.includes(id) ? 'liked heart-like-button' : 'heart-like-button'
+  const loveIcon = (id, Favs) => {
+    const iconClass = Favs.includes(id) ? 'liked heart' : 'heart'
     return (
-      <div className="Love" onClick={() => this.loveClick(iconId, id)}>
-        <div id={iconId} className={iconClass}></div>
+      <div className="Love" onClick={() => loveClick(id)}>
+        <div ref={ref} className={iconClass}></div>
       </div>
     )
   }
 
-  loveClick = (iconId, id) => {
-    const button = document.querySelector(`#${iconId}`);
-    this.props.Love(id)
-    if (button.classList.contains("liked")) {
-      button.classList.remove("liked");
-    } else {
-      button.classList.add("liked");
-    }
+  const loveClick = (id) => {
+    props.Love(id)
+    ref.current.classList.toggle('liked')
   }
 
-  leftIcon = (id) => {
-    const { Type, leftIconClick } = this.props
+  const leftIcon = (id) => {
+    const { Type, leftIconClick } = props
     if (Type !== 'trash') return
     return (
-      <SlIcon
-        className="Icon leftIcon"
-        name='arrow-90deg-left'
-        onClick={() => leftIconClick(id)}
-        style={{ fontSize: 'var(--sl-font-size-medium)' }}
-      />
+      <div className="options leftIcon">
+        <SlIcon
+          name='arrow-90deg-left'
+          onClick={() => leftIconClick(id)}
+          style={{ fontSize: 'var(--sl-font-size-medium)' }}
+        />
+      </div>
     )
   }
 
-  rightIcon = (id) => {
-    const { Type, rightIconClick } = this.props
-    if (Type === 'home') return this.paletteMenu(id)
+  const rightIcon = (id) => {
+    const { Type, rightIconClick } = props
+    if (Type === 'home') return paletteMenu(id)
     let icon
     if (Type === 'trash') icon = 'trash'
     if (Type === 'hidden') icon = 'eye-fill'
     if (Type === 'favourite') icon = 'heart'
     return (
-      <SlIcon
-        className='Icon rightIcon'
-        name={icon}
-        onClick={() => rightIconClick(id)}
-      />
+      <div className="options rightIcon">
+        <SlIcon
+          name={icon}
+          onClick={() => rightIconClick(id)}
+        />
+      </div>
     )
   }
 
-  paletteMenu = (id) => {
-    const { Type, Hide, Delete } = this.props
+  const paletteMenu = (id) => {
+    const { Type, Hide, Delete } = props
     if (Type !== 'home') return
     return <SlDropdown size='small'>
-      <div className="options" slot='trigger'>
-        <SlIcon className='icon'  name='three-dots' />
+      <div className="options rightIcon" slot='trigger'>
+        <SlIcon className='icon' name='three-dots' />
       </div>
       <SlMenu>
         <SlMenuItem className='menu-item'>Edit
@@ -105,6 +98,8 @@ class MiniPalette extends PureComponent {
       </SlMenu>
     </SlDropdown>
   }
+
+  return render()
 }
 
 export default MiniPalette
